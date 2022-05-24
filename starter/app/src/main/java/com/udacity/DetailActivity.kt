5e4d9a3e50e78dfe94d1e301ae1c093ad686b926
trunghieuluongit <trunghieuluongit@gmail.com>
2022-05-24 10:35:11 +0700

    Final Project

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +12,16 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        if(intent?.extras != null){
+            fileNameTextView.text = intent.getStringExtra("fileName")
+            statusTextView.text = intent.getStringExtra("status")
+        }
+
+        ok_button.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
 
 }
